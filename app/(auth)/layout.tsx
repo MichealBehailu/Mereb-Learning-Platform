898b2296a42873dce90daff88c47970bac8a709d
Micheal <michealbehailu94@gmail.com>
2026-02-19 23:25:21 +0300

    Finished the login component UI

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,11 @@
 //this is a layout for the auth(login and signup) pages
 
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import Logo2 from '@/public/logo2.png'
+
 export default function AuthLayout({
   children,
 }: {
@@ -7,7 +13,20 @@ export default function AuthLayout({
 }) {
   return (
     <div className="realtive flex min-h-svh flex-col items-center justify-center">
-      <div className="flex w-full max-w-sm flex-col gap-6">{children}</div>
+      <Link href="/" className={buttonVariants({
+        variant:'outline',
+        className:"absolute top-4 left-4"
+      })}> <ArrowLeft className="size-4"/>Back</Link>
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link href='/' className="flex items-center gap-2 self-center font-medium">
+         <Image src={Logo2} alt="logo" width={32} height={32} />
+         Mereb Learning.
+        </Link>
+        {children}
+        <div className="text-balance text-center text-xs text-muted-foreground">
+          By clicking continue, you agree to our  <span className="hover:text-primary hover:underline cursor-pointer">Terms of Service</span> and <span className="cursor-pointer hover:text-primary hover:underline">Privacy Policy</span>.
+        </div>
+        </div>
     </div>
   );
 }
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -6,6 +6,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { GithubIcon } from "lucide-react";
 import React from "react";
 
@@ -16,7 +18,7 @@ export default function LoginPage() {
         <CardTitle className="text-xl">Welcome back!</CardTitle>
         <CardDescription>Login with your Github Email Account</CardDescription>
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex flex-col gap-4">
         <Button className="w-full" variant="outline">
           <GithubIcon className="size-4" />
           Sign in with Github
@@ -24,6 +26,14 @@ export default function LoginPage() {
 
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
           <span className="relative z-10 bg-card px-2 text-mute-foreground"> Or continue with</span>
+        </div>
+
+        <div className="grid gap-3">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" placeholder="m@example.com"/>
+          </div>
+          <Button>Continue with Email</Button>
         </div>
       </CardContent>
     </Card>
