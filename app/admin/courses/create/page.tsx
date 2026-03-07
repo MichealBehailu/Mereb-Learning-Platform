21d8b730c8c5463a988abe8a30b4065609ae62c9
Micheal <michealbehailu94@gmail.com>
2026-03-07 12:23:52 +0300

    creating validation schema for course form

---
+++ app/admin/courses/create/page.tsx
@@ -0,0 +1,28 @@
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+
+export default function CourseCreationPage(){
+    return (
+        <>
+            <div className="flex items-center gap-4">
+                <Link href="/admin/courses" className={buttonVariants({
+                    variant:"outline",
+                    size:"icon"
+                })}>
+                  <ArrowLeft className="size-4"/>
+                </Link>
+                <h1 className="text-2xl font-bold">Create Courses</h1>
+            </div>
+
+            <Card>
+                <CardHeader>
+                    <CardTitle>Basic Information</CardTitle>
+                    <CardDescription>Fill in the basic information for your course</CardDescription>
+                </CardHeader>
+            </Card>
+
+        </>
+    )
+}
---
+++ lib/zodSchemas.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const courseLevels = ['Beginner', 'Intermediate', 'Advanced'] as const;
+
+export const courseStatus = ['Draft', 'Published', 'Archived'] as const;
+
+export const courseSchema = z.object({
+    title: z.string().min(3, {message:'Title must be at least 3 characters long'}).max(100,{message:'Title must be at most 100 characters long'}),
+    description: z.string().min(3, {message:'Description must be at least 3 characters long'}),
+    fileKey: z.string().min(1, {message:'File key is required'}),
+    price: z.coerce.number().min(1, {message:'Price must be positive number'}), //i set it because i dont want the user to create free courses
+    duration:z.coerce.number().min(1, {message:'Duration must be atleast 1 hour'}).max(500, {message:'Duration must be at most 500 hours'}),
+    level: z.enum(courseLevels, {message:'Level is required'}),
+    category: z.string(),
+    smallDescription: z.string().min(3, {message:'Small description must be at least 3 characters long'}).max(200, {message:'Small description must be at most 200 characters long'}),
+    slug: z.string().min(3, {message:'Slug must be at least 3 characters long'}),
+    status: z.enum(courseStatus, {message:'Status is required'}),
+});
+
