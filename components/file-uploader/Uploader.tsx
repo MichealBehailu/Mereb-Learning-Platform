5bfaf958f51f836e2a3717d1cbe402a913590ff2
Micheal <michealbehailu94@gmail.com>
2026-03-17 17:27:32 +0300

    Styling the drag and drop

--- components/file-uploader/Uploader.tsx
+++ components/file-uploader/Uploader.tsx
@@ -1,6 +1,8 @@
 'use client'
 import {useCallback} from 'react'
 import {useDropzone} from 'react-dropzone'
+import {Card, CardContent} from '@/components/ui/card'
+import {cn} from '@/lib/utils'
 
 export function Uploader() {
      const onDrop = useCallback((acceptedFiles:File[]) => {
@@ -9,14 +11,16 @@ export function Uploader() {
   }, [])
     const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
     return (
-       <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
-    </div>
+       <Card {...getRootProps()} className={cn(
+        "relative border-2 border-dashed transition-colors duration-200 ease-in-out w-full h-64",
+        isDragActive ? 'border-primary bg-primary/10 border-solid' : 'border-border hover:border-primary'
+       )}>
+      <CardContent className='flex items-center justify-center h-full w-full p-4'>
+          <input {...getInputProps()} />
+         <p>upload a file</p>
+      </CardContent>
+    </Card>
     )
 }
 
+//for the uploader i used a simple package called react dropzone 
