292787bf60622d613924e40b7c8289bb8f8526f1
Micheal <michealbehailu94@gmail.com>
2026-03-30 21:53:30 +0300

    Building the upload route

---
+++ app/api/s3/upload/route.ts
@@ -0,0 +1,28 @@
+import {z} from 'zod'
+import { NextResponse } from 'next/server'
+
+const fileUploadSchema = z.object({
+    fileName: z.string().min(1, {message: 'File name is required'}),
+    contentType: z.string().min(1, {message: 'Content type is required'}),
+    size: z.number().min(1, {message: 'Size is required'}),
+    isImage: z.boolean()
+})
+
+
+export function POST(request: Request){
+    
+    try {
+        const body = request.body; //to get the boady input but we need some kind of validation //so we create a schema called fileUploadSchema 
+        const validation = fileUploadSchema.safeParse(body) //check if the body that is sent as an input validates the schema
+
+        if(!validation.success){ //if the body fails to validate the schema then validation.sucess will be false
+            return NextResponse.json({error: 'Invalid request body'}, {status: 400}) //400 means failed 
+        }
+
+        
+        
+    } catch (error) {
+        
+    }
+
+}
