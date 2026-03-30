60bf702a87b9a9761b5994c26664418d1a46c5ab
Micheal <michealbehailu94@gmail.com>
2026-03-30 23:08:15 +0300

    Finishing the route for the S3

--- app/api/s3/upload/route.ts
+++ app/api/s3/upload/route.ts
@@ -1,5 +1,9 @@
 import {z} from 'zod'
 import { NextResponse } from 'next/server'
+import { PutObjectCommand } from '@aws-sdk/client-s3'
+import {v4 as uuidv4} from 'uuid'
+import {getSignedUrl} from '@aws-sdk/s3-request-presigner'
+import {S3} from '@lib/S3Client'
 
 const fileUploadSchema = z.object({
     fileName: z.string().min(1, {message: 'File name is required'}),
@@ -19,10 +23,27 @@ export async function POST(request: Request){
             return NextResponse.json({error: 'Invalid request body'}, {status: 400}) //400 means failed 
         }
 
-        const {fileName, contentType, size, isImage} = validation.data
+        const {fileName, contentType, size, isImage} = validation.data;
+
+        const uniqueKey = `${uuidv4()}-${fileName}`; //unique id for the file //the output is like the key then the name of the file
+
+        const command = new PutObjectCommand({
+            Bucket:process.env.NEXT_PUBLIC_S3_BUCKET_NAME_IMAGES,
+            ContentType: contentType,
+            ContentLength:size,
+            Key: uniqueKey //it is like ID
+        })
         
+        const presignedUrl = await getSignedUrl(S3,command, {
+            expiresIn:360, //URL expires in 6min //in second(represented) in this case it is 6 minutes
+
+        })
+
+        const response = {presignedUrl, key:uniqueKey}
+
+        return NextResponse.json(response)
     } catch (error) {
-        
+        return NextResponse.json({error: 'Failed to generate presigned URL'}, {status: 500})
     }
 
 } 
